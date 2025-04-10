bb5b723f1ffbf349294ecc0c86b86232ce71e8c9
erginbilgin0 <erginbilgin0@gmail.com>
2025-04-10 09:39:42 +0300

    ANG2 pages are created

--- proje-adi/src/app/app.component.ts
+++ proje-adi/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UserCardComponent } from './user-card/user-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,UserCardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular Workshop';
-  props = {"productId":1235}
 }
--- proje-adi/src/app/app.routes.ts
+++ proje-adi/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from '../app/pages/homepage/homepage.component';
+import { AboutComponent } from '../app/pages/about/about.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+    title: 'Ana Sayfa'
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    title: 'Hakkında'
+  }
+  
+];
